e4d635a745b5985e925e3fd34f16b4bdbda5eed1
panadolextra91 <126870223+panadolextra91@users.noreply.github.com>
2024-11-27 18:10:11 +0700

    Input.jsx added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { LuCat } from "react-icons/lu";
 import TopButtons from './components/TopButtons';
-
+import Inputs from './components/Inputs';
 const App = () => {
   return (
     <div className="max-w-screen-lg my-5 mx-auto py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-600">
       <TopButtons/>
+      <Inputs/>
     </div>
   )
 };
---
+++ src/components/Inputs.jsx
@@ -0,0 +1,8 @@
+
+const Inputs = () => {
+  return (
+    <div>Inputs</div>
+  )
+}
+
+export default Inputs;
